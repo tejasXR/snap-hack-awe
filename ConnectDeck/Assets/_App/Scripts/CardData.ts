d585dd61b86f72a85278b12e1ed73e2855eb1854
tejasXR <tejasshroff24@gmail.com>
2025-06-07 18:17:41 -0400

    Adding header ice breaker string data to CardData

--- ConnectDeck/Assets/_App/Scripts/CardData.ts
+++ ConnectDeck/Assets/_App/Scripts/CardData.ts
@@ -4,5 +4,6 @@ export class CardData extends BaseScriptComponent {
     @input cardName: Material;
     @input cardFace: Material;
     @input details: string;
-    @input iceBreaker:string;
+    @input iceBreaker: string;
+    @input headerIceBreaker: string;
 }
