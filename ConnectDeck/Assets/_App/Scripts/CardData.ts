7e943684cf2dcc77c4d4ef03943cb3635b65af42
tejasXR <tejasshroff24@gmail.com>
2025-06-07 23:48:42 -0400

    Adding more card data

--- ConnectDeck/Assets/_App/Scripts/CardData.ts
+++ ConnectDeck/Assets/_App/Scripts/CardData.ts
@@ -3,6 +3,7 @@ export class CardData extends BaseScriptComponent {
     
     @input cardName: Material;
     @input cardFace: Material;
+    @input cardMinimized: Material;
     @input details: string;
     @input iceBreaker: string;
     @input headerIceBreaker: string;
