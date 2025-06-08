272c55823959e5ce4070095123f7885ee82af830
tejasXR <tejasshroff24@gmail.com>
2025-06-07 23:47:58 -0400

    Improving palm interaction to enable dual hand tracking

--- ConnectDeck/Assets/_App/Scripts/OpenPalmInteraction.ts
+++ ConnectDeck/Assets/_App/Scripts/OpenPalmInteraction.ts
@@ -1,36 +1,82 @@
+import { CardController } from "_App/Scripts/CardController";
+
 @component
 export class OpenPalmInteraction extends BaseScriptComponent {
 
     private gestureModule: GestureModule = require('LensStudio:GestureModule');
 
+    @input cardController: CardController
+
     @input leftHand: SceneObject;
+    @input rightHand: SceneObject;
+
     @input cardUiOffset: vec3;
     @input cardUi: SceneObject;
 
-    onAwake() {
-    
-        // Follow hand
+    @input compassOffset: vec3;
+    @input compass: SceneObject;
+
+    constructor()
+    {
+        super();
+
+        // this.createEvent('OnEnableEvent').bind(() => this.onEnable());
+        // this.createEvent('OnStartEvent').bind(() => this.onStart());
+    }
+
+    onAwake()
+    {
+        this.cardController.onConnectionSentCallback = () => {
+            this.onStartTracking();
+        }
+
+        this.cardUi.enabled = false;
+        this.compass.enabled = false;
+    }
+
+    private onStartTracking() 
+    {
+        this.cardUi.enabled = true;
+        this.compass.enabled = true;
+
+        // -- Card UI --
         this.gestureModule
         .getTargetingDataEvent(GestureModule.HandType.Left)
         .add((targetArgs: TargetingDataArgs) => {
 
-            var xOffset = this.leftHand.getTransform().right.x * this.cardUiOffset.x;
             var yOffset = this.leftHand.getTransform().up.y * this.cardUiOffset.y;
-            var zOffset = this.leftHand.getTransform().forward.z * this.cardUiOffset.z;
             
             var destPoint = new vec3
             (
             targetArgs.rayOriginInWorld.x, 
-            targetArgs.rayOriginInWorld.y + yOffset, 
+            targetArgs.rayOriginInWorld.y - yOffset, 
             targetArgs.rayOriginInWorld.z
             );
-
-            print(this.leftHand.getTransform().up);
 
             var isLeftPalmUp = this.leftHand.getTransform().up.y <= -.4;
             this.cardUi.enabled = isLeftPalmUp;
 
             this.cardUi.getTransform().setWorldPosition(destPoint);
+        });
+
+        // -- Wayfinder UI --
+        this.gestureModule
+        .getTargetingDataEvent(GestureModule.HandType.Right)
+        .add((targetArgs: TargetingDataArgs) => {
+
+            var yOffset = this.rightHand.getTransform().up.y * this.compassOffset.y;
+            
+            var destPoint = new vec3
+            (
+            targetArgs.rayOriginInWorld.x, 
+            targetArgs.rayOriginInWorld.y - yOffset, 
+            targetArgs.rayOriginInWorld.z
+            );
+
+            var isRightPalmUp = this.rightHand.getTransform().up.y <= -.4;
+            this.compass.enabled = isRightPalmUp;
+
+            this.compass.getTransform().setWorldPosition(destPoint);
         });
     }
 }
